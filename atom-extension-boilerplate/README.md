7881e5cfc5b59052ccc63ff0496c48879287adc4
Joern Konopka <cldx3000@googlemail.com>
2014-04-27 16:18:49 +0200

    Several Mockups for Design Discussion and stuff

---
+++ atom-extension-boilerplate/README.md
@@ -0,0 +1,23 @@
+Atom Extension
+=========
+Description
+
+![initial version screenshot](add image url)
+
+### Compatibility
+
+Gnome Shell 3.10 and 3.12
+
+### Installation
+
+Currently, the extension can only be installed from source. To install, download latest zip from GitHub or clone the repo with git
+
+`git clone https://github.com/ozonos/atom-extension.git`
+
+Then run
+
+`make install`
+
+### License
+
+This project is licensed with GPL version 3 and later. See LICENSE for more details.
---
+++ atom-extension-boilerplate/extension.js
@@ -0,0 +1,31 @@
+// -*- mode: js; js-indent-level: 4; indent-tabs-mode: nil -*-
+/*jshint esnext: true */
+/*jshint indent: 4 */
+
+/* Numix/Ozon Project 2014
+ * 
+ * Name: Atom Extension
+ * UUID: atom-extension@ozonos.org
+ * Version: 0.1
+ */
+
+/* Extension Skeleton Imports */
+/* what exactly does this get us? */
+const ExtensionUtils = imports.misc.extensionUtils;
+const Me = ExtensionUtils.getCurrentExtension();
+/* This should includ convenience and such */
+
+/* Extension Imports */
+
+
+function init() {
+	/* NO CODE IN INIT */
+}
+
+function enable() {
+	/* Properly initiate the Extension */
+}
+
+function disable() {
+	/* Properly disable the extension, Clean Up behind you */
+}
