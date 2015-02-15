6aa7f815e04b2ad55cb04ed7f59d690f2a113aab
Satyajit Sahoo <satyajit.happy@gmail.com>
2015-02-16 01:47:38 +0530

    Add a hamburger menu

--- js/main.js
+++ js/main.js
@@ -4,8 +4,17 @@
 $(function() {
     'use strict';
 
-    var $downloadButton = $('#button-download'),
+    var $navBar = $('nav'),
+        $downloadButton = $('#button-download'),
         $warningDialog = $('<div>').html($('#warning-dialog-content').html());
+
+    $(document).on('click', function(e) {
+        if ($(e.target).closest('.nav-menu').length) {
+            $navBar.toggleClass('nav-open');
+        } else if (!$(e.target).closest('.nav-right').length) {
+            $navBar.removeClass('nav-open');
+        }
+    });
 
     $downloadButton.on('click', function() {
         $warningDialog.modal();
